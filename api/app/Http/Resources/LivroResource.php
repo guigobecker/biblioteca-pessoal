41f72b1849129e3b85c931aee6d15e7270ef3c56
Rodrigo Becker <rsbecker@inf.ufsm.br>
2023-10-27 23:23:38 -0300

    Formatar data de adição na API com Carbon

--- api/app/Http/Resources/LivroResource.php
+++ api/app/Http/Resources/LivroResource.php
@@ -3,6 +3,7 @@
 namespace App\Http\Resources;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class LivroResource extends JsonResource
@@ -20,7 +21,7 @@ class LivroResource extends JsonResource
             'autor' => $this->autor,
             'classificacao' => $this->classificacao,
             'resenha' => $this->resenha,
-            'criado_em' => $this->created_at,
+            'criado_em' => Carbon::parse($this->created_at)->format('d/m/Y H:i:s'),
         ];
     }
 }

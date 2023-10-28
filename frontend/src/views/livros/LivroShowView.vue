b84a95facbfaadfacaae28409c072dfa02d5f2c3
Rodrigo Becker <rsbecker@inf.ufsm.br>
2023-10-27 22:52:57 -0300

    Implementar método de deletar

--- frontend/src/views/livros/LivroShowView.vue
+++ frontend/src/views/livros/LivroShowView.vue
@@ -2,7 +2,7 @@
 import useLivros from "../../composables/livros";
 import { onMounted } from "vue";
 
-const { livro, getLivro, errors } = useLivros();
+const { livro, getLivro, errors, destroyLivro } = useLivros();
 
 const props = defineProps({
   id: {
@@ -22,7 +22,11 @@ onMounted(() => getLivro(props.id));
     <h4 class="mb-3 text-l font-medium tracking-tight text-gray-900">
       {{ livro.autor }}
     </h4>
+    <h4 class="mb-3 text-l font-medium tracking-tight text-gray-900">
+      {{ livro.classificacao + '/5' }}
+    </h4>
     <p class="mb-3 font-normal text-gray-700">{{ livro.resenha }}</p>
+    <p class="mb-3 text-sm font-normal text-gray-700">{{ livro.criado_em }}</p>
     <div class="space-x-2">
       <routerLink
         :to="{ name: 'edit', params: { id: livro.id } }"
@@ -31,6 +35,7 @@ onMounted(() => getLivro(props.id));
         Editar
       </routerLink>
       <button
+        @click="destroyLivro(livro.id)"
         class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-red-700 rounded-lg hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300"
       >
         Deletar
